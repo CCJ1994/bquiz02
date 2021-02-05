aad96a780d416feb97aba4f1b104d02a66452bab
ccj1994 <a0changj2@gmail.com>
2021-02-05 08:25:09 +0800

    第二次練習

--- api/get_news.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include_once "../base.php";
-
-$news=$News->find($_GET['id']);
-echo nl2br($news['text']);
-?>
--- api/get_title.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-include_once "../base.php";
-
-$news=$News->all(['type'=>$_GET['type']]);
-foreach ($news as $new) {
-  echo "<a href='javascript:getNews({$new['id']})' style='display:block;'>{$new['title']}</a>";
-}
-?>
